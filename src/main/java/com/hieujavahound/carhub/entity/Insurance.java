54d9389f9aca8c7d6ded9f08efd64fa95557ebbb
Hieu Nguyen <HieuJames204@gmail.com>
2025-12-27 19:14:34 +0700

    Add Insurance class

--- src/main/java/com/hieujavahound/carhub/entity/Insurance.java
+++ src/main/java/com/hieujavahound/carhub/entity/Insurance.java
@@ -1,5 +1,37 @@
 package com.hieujavahound.carhub.entity;
 
+import com.hieujavahound.carhub.enums.InsuranceType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "insurance")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Insurance {
-    // TODO: Create Insurance class
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private InsuranceType insuranceType;
+
+    @Column(nullable = false)
+    private double coverageAmount;
+
+    @Column(nullable = false)
+    private double dailyRate;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToMany(mappedBy = "insurances")
+    List<Rental> rentals = new ArrayList<>();
 }
